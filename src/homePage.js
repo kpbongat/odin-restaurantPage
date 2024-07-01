e1572683cd6ff8ab962eff3fc95a8a7388127215
kpbongat <kpbongat.dev@gmail.com>
2024-07-01 17:12:15 +0800

    Fix wrong header from about to home

--- src/homePage.js
+++ src/homePage.js
@@ -1,9 +1,9 @@
 function homePage() 
 {
 const contentDiv = document.querySelector('div#content');
-contentDiv.setAttribute('id', 'about');
+contentDiv.setAttribute('id', 'home');
 const headerText = document.createElement('h1');
-headerText.textContent = 'About';
+headerText.textContent = 'Home';
 contentDiv.appendChild(headerText);
 const bodyText = document.createElement('p');
 bodyText.textContent = 'Steve\'s is a celebration of quality.  Our commitment to sourcing the finest ingredients, from meticulously raised livestock and sustainably caught seafood to seasonal produce at its peak, is the foundation of our culinary philosophy.  Experience the difference in every dish, where freshness and flavor take center stage.';
